2dd6b8380f0723e53df3f9315ca7e81c7a7bab16
Aaron <chuang.chi.ju@gmail.com>
2025-04-17 13:51:54 +0800

    update

--- images/[[path]].js
+++ images/[[path]].js
@@ -1,11 +1,6 @@
- 
-/**
- * Pages Function 處理 GET 請求
- * @param {object} context - 包含請求、環境變數、參數等的上下文物件
- */
 export async function onRequestGet(ctx) {
   const path = new URL(ctx.request.url).pathname.replace("/images/", "");
-  const file = await ctx.env.MEDIA.get(path);
+  const file = await ctx.env.IMAGES.get(path);
   if (!file) return new Response(null, { status: 404 });
   return new Response(file.body, {
     headers: { "Content-Type": file.httpMetadata.contentType },
